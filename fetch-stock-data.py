94ac1eda6e4c8cf04ce0a632f65c86981c220373
Debjit Ghosh <darkiceberg@gmail.com>
2018-05-31 14:47:44 -0700

    Updating per stock per time period instead of per stock through entire time period

--- fetch-stock-data.py
+++ fetch-stock-data.py
@@ -22,7 +22,7 @@ url_get_companies = 'https://api.intrinio.com/companies'
 auth_user = '48e36a026502830319d748727218a21f'
 auth_pass = 'f671948d2171caac783fa422dfa42bfe'
 database_name = "stocks"
-companies_of_interest = ["AAC", "AAL", "AAMC", "AAME", "AAPL"]
+companies_of_interest = ["AAL", "AAME", "AAPL"]
 
 client = InfluxDBClient(host='localhost', port=8086)	
 client.create_database(database_name)
@@ -32,18 +32,18 @@ def get_data():
 
 	client.switch_database(database_name)
 	
-	#Read each of the 5 companies
-	for company in companies_of_interest:
+	#Initializing the start date of fetching stocks data to January 1st, 2010
+	start_date = date(2015,1,1)
+	
+	#Initializing the end date to be 100 days from the start date as that's how much
+	#data permits us to retrieve at a time
+	end_date = start_date + timedelta(days = 100)
 
-		#Initializing the start date of fetching stocks data to January 1st, 2010
-		start_date = date(2015,1,1)
-		
-		#Initializing the end date to be 100 days from the start date as that's how much
-		#data permits us to retrieve at a time
-		end_date = start_date + timedelta(days = 100)
-		
-		for j in range(24):
+	for month in range(36):
 
+		#Read each of the 5 companies
+		for company in companies_of_interest:
+		
 			request_obj = requests.get(url_get_historical_data + company + '&item=pricetoearnings&start_date=' + str(start_date) + '&end_date=' + str(end_date), auth=HTTPBasicAuth(auth_user, auth_pass))
 
 			json_data = json.loads(request_obj.text)
@@ -64,7 +64,7 @@ def get_data():
 
 				pointValues = {
 				    "time": json_data["data"][i]["date"],
-				    "measurement": 'stocks5',
+				    "measurement": 'stocks',
 				    "tags": {
 					"Symbol" : company,
 				    },
@@ -78,9 +78,11 @@ def get_data():
 			print(series)
 			client.write_points(series)
 			print ("Data written to InfluxDB to database demo and series stocks {} {}".format(company,len(series)))
-			start_date = end_date
-			end_date = end_date + timedelta(days = 30)
+
 			time.sleep(1)
+
+		start_date = end_date
+		end_date = end_date + timedelta(days = 30)
 		
 	return ("Stocks data has been written to InfluxDB.")
 
